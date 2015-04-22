a55a596370ed039b6ce6b4dd3e5e507c694a4e67
dfleider <dfleiderman@gmail.com>
2015-04-22 16:06:09 -0300

    ultima subida

--- MenuExp.java
+++ MenuExp.java
@@ -17,47 +17,47 @@ public class MenuExp extends JFrame {
         setJMenuBar(menuBar);
         
         // Define and add two drop down menu to the menubar
-        JMenu fileMenu = new JMenu("File");
-        JMenu editMenu = new JMenu("Edit");
+        JMenu fileMenu = new JMenu("Archivo");
+        JMenu editMenu = new JMenu("Editar");
         menuBar.add(fileMenu);
         menuBar.add(editMenu);
         
         // Create and add simple menu item to one of the drop down menu
-        JMenuItem newAction = new JMenuItem("New");
-        JMenuItem openAction = new JMenuItem("Open");
-        JMenuItem exitAction = new JMenuItem("Exit");
-        JMenuItem cutAction = new JMenuItem("Cut");
-        JMenuItem copyAction = new JMenuItem("Copy");
-        JMenuItem pasteAction = new JMenuItem("Paste");
+        JMenuItem nuevaAccion = new JMenuItem("Nuevo");
+        JMenuItem abrirAccion = new JMenuItem("Abrir");
+        JMenuItem salirAction = new JMenuItem("Salir");
+        JMenuItem cortarAction = new JMenuItem("Cortar");
+        JMenuItem copiarAction = new JMenuItem("Copiar");
+        JMenuItem pegarAction = new JMenuItem("Pegar");
         
         // Create and add CheckButton as a menu item to one of the drop down
         // menu
-        JCheckBoxMenuItem checkAction = new JCheckBoxMenuItem("Check Action");
+        JCheckBoxMenuItem checkAction = new JCheckBoxMenuItem("Guardar Variable");
         // Create and add Radio Buttons as simple menu items to one of the drop
         // down menu
-        JRadioButtonMenuItem radioAction1 = new JRadioButtonMenuItem(
-                "Radio Button1");
-        JRadioButtonMenuItem radioAction2 = new JRadioButtonMenuItem(
-                "Radio Button2");
+        JRadioButtonMenuItem graficar = new JRadioButtonMenuItem(
+                "Graficar");
+        JRadioButtonMenuItem borrar = new JRadioButtonMenuItem(
+                "Borrar");
         // Create a ButtonGroup and add both radio Button to it. Only one radio
         // button in a ButtonGroup can be selected at a time.
         ButtonGroup bg = new ButtonGroup();
-        bg.add(radioAction1);
-        bg.add(radioAction2);
-        fileMenu.add(newAction);
-        fileMenu.add(openAction);
+        bg.add(graficar);
+        bg.add(borrar);
+        fileMenu.add(nuevaAccion);
+        fileMenu.add(abrirAccion);
         fileMenu.add(checkAction);
         fileMenu.addSeparator();
-        fileMenu.add(exitAction);
-        editMenu.add(cutAction);
-        editMenu.add(copyAction);
-        editMenu.add(pasteAction);
+        fileMenu.add(salirAction);
+        editMenu.add(cortarAction);
+        editMenu.add(copiarAction);
+        editMenu.add(pegarAction);
         editMenu.addSeparator();
-        editMenu.add(radioAction1);
-        editMenu.add(radioAction2);
+        editMenu.add(graficar);
+        editMenu.add(borrar);
         // Add a listener to the New menu item. actionPerformed() method will
         // invoked, if user triggred this menu item
-        newAction.addActionListener(new ActionListener() {
+        nuevaAccion.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent arg0) {
                 System.out.println("You have clicked on the new action");
             }
